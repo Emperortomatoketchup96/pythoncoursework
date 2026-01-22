6a30d49d18f0ff2ff0126c405e7c740f2cf97851
Oliver <oliver_bratteng422@sunyempire.edu>
2026-01-21 22:59:33 -0500

    Add files via upload

---
+++ firstassignment.py
@@ -0,0 +1,11 @@
+# 1. Greeting
+name = "Oliver"
+print(f"Hello my name is {name} and I am taking a class to learn Python")
+
+# 2. Math problem
+number1 = 10
+number2 = 32
+result = number1 + number2
+
+# 3. Ending result
+print(f"The answer to {number1} +{number2} is {result}.")
